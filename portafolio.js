094c0e06317e05ca80721003b01dcc6e2cc01eee
Loreto Maturana <54029306+ltmaturana@users.noreply.github.com>
2019-08-19 10:33:49 -0400

    ver más

--- portafolio.js
+++ portafolio.js
@@ -3,49 +3,49 @@ var trabajos = [{
     alt: "Descripción de la imagen",
     title: "Cyl",
     about: "Banca de madera de terciado de 3 mm, pintada y sellada con carbonileo",
-    url: "www.emol.cl"
+    url: "http://www.instagram.com/brnwld"
 }, {
     figure: "media/trabajo-02-taller.jpg",
     alt: "Descripción de la imagen",
     title: "Título de segundo trabajo",
     about: "Texto simulado de descripción de segundo trabajo",
-    url: "www.emol.cl"
+    url: "https://www.emol.cl"
 }, {
     figure: "media/trabajo-03-zoologico.jpg",
     alt: "Descripción de la imagen",
     title: "Dispositivo de Enriquecimiento Animal para Servales",
     about: "Texto simulado de descripción de tercer trabajo",
-    url: "www.emol.cl"
+    url: "https://www.emol.cl"
 }, {
     figure: "media/trabajo-04-editorial.jpg",
     alt: "Descripción de la imagen",
     title: "Título de cuarto trabajo",
     about: "Texto simulado de descripción de cuarto trabajo",
-    url: "www.emol.cl"
+    url: "https://www.emol.cl"
 }, {
     figure: "media/trabajo-05-artesania.jpg",
     alt: "Descripción de la imagen",
     title: "Título de quinto trabajo",
     about: "Texto simulado de descripción de quinto trabajo",
-    url: "www.emol.cl"
+    url: "https://www.emol.cl"
 }, {
     figure: "media/trabajo-06-textil.jpg",
     alt: "Descripción de la imagen",
     title: "Título de sexto trabajo",
     about: "Texto simulado de descripción de sexto trabajo",
-    url: "www.emol.cl"
+    url: "https://www.emol.cl"
 }, {
     figure: "media/trabajo-07-materiales.jpg",
     alt: "Descripción de la imagen",
     title: "Título de séptimo trabajo",
     about: "Texto simulado de descripción de séptimo trabajo",
-    url: "www.emol.cl"
+    url: "https://www.emol.cl"
 }];
 // LUEGO CREAMOS PARTES DEL SITIO CON CADA DATO CONTENIDO EN LA VARIABLE
 $.each(trabajos, function(i, val) {
     var uno = '<figure><img src="' + trabajos[i].figure + '" alt="' + trabajos[i].alt + '"/></figure>';
     var dos = '<h4>' + trabajos[i].title + '</h4>';
     var tres = '<p>' + trabajos[i].about + '</p>';
-    var cuatro = '<p><a href="'+ trabajos[i].url + '">Ver más</a></p>';
-    $('#portafolio').append('<div class="item">' + uno + dos + tres + '</div>')
+    var cuatro = '<p><a href="' + trabajos[i].url + '">Ver más</a></p>';
+    $('#portafolio').append('<div class="item">' + uno + dos + tres + cuatro + '</div>')
 });
