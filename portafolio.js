0059f3dcf34bcda7294ebae5bbb67787e56aab57
vjcarrasco <53921874+vjcarrasco@users.noreply.github.com>
2019-08-24 11:50:04 -0400

    portafolio centrado + edit de los integrantes

--- portafolio.js
+++ portafolio.js
@@ -29,6 +29,5 @@ $.each(trabajos, function(i, val) {
     var uno = '<figure><img src="' + trabajos[i].figure + '" alt="' + trabajos[i].alt + '"/></figure>';
     var dos = '<h4>' + trabajos[i].title + '</h4>';
     var tres = '<p>' + trabajos[i].about + '</p>';
-    var cuatro = '<p><a href="' + trabajos[i].url + '">Ver más</a></p>';
-    $('#portafolio').append('<div class="item">' + uno + dos + tres + cuatro + '</div>')
+    $('#portafolio').append('<div class="item">' + uno + dos + tres + '</div>')
 });
