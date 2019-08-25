911ffc751a7f2c7498c0977c8fdec3e906258983
Loreto Maturana <54029306+ltmaturana@users.noreply.github.com>
2019-08-25 11:54:52 -0400

    Video cambiado, algunas fotos
    aun falta bajar el tamaño de varias fotos
    faltan descripciones
    la foto de belén
    y ver si cambiamos el color del nombre o algu

--- portafolio.js
+++ portafolio.js
@@ -1,115 +1,115 @@
 var trabajosValentina= [{
-    figure: "media/trabajo-01-estructuras.jpg",
+    figure: "media/trabajo-01-lolo.jpg",
     alt: "Descripción de la imagen",
     title: "Cyl",
-    about: "Banca de madera de terciado de 3 mm, pintada y sellada con carbonileo",
-    url: "http://www.instagram.com/brnwld"
+    about: "Banca fabricada a partir de placas de terciado de 3 milímetros. La banca tiene solo dos puntos de apoyo y reforzada por un sistema de costillas y ensambles, y estructurada con cables zip.",
+
 }, {
-    figure: "media/trabajo-02-taller.jpg",
+    figure: "media/trabajo-01-vale.jpg",
+    alt: "Descripción de la imagen",
+    title: "Agrocinta",
+    about: "Dispositivo consistente en dos partes, la primera, una cinta de papel biodegradable, con sem",
+
+}, {
+    figure: "media/trabajo-02-vale.jpg",
+    alt: "Descripción de la imagen",
+    title: "Ilko Basics",
+    about: "Texto simulado de descripción de tercer trabajo",
+
+}, {
+    figure: "media/trabajo-03-vale.jpeg",
+    alt: "Descripción de la imagen",
+    title: "Maty",
+    about: "Banca de madera de terciado de 3 mm, pintada y sellada con carbonileo",
+
+}, {
+    figure: "media/trabajo-04-vale.jpg",
     alt: "Descripción de la imagen",
     title: "Título de segundo trabajo",
     about: "Texto simulado de descripción de segundo trabajo",
-    url: "https://www.emol.cl"
+
 }, {
     figure: "media/trabajo-03-zoologico.jpg",
     alt: "Descripción de la imagen",
     title: "Dispositivo de Enriquecimiento Animal para Servales",
     about: "Texto simulado de descripción de tercer trabajo",
-    url: "https://www.emol.cl"
-}, {
-    figure: "media/trabajo-01-estructuras.jpg",
-    alt: "Descripción de la imagen",
-    title: "Cyl",
-    about: "Banca de madera de terciado de 3 mm, pintada y sellada con carbonileo",
-    url: "http://www.instagram.com/brnwld"
-}, {
-    figure: "media/trabajo-02-taller.jpg",
-    alt: "Descripción de la imagen",
-    title: "Título de segundo trabajo",
-    about: "Texto simulado de descripción de segundo trabajo",
-    url: "https://www.emol.cl"
-}, {
-    figure: "media/trabajo-03-zoologico.jpg",
-    alt: "Descripción de la imagen",
-    title: "Dispositivo de Enriquecimiento Animal para Servales",
-    about: "Texto simulado de descripción de tercer trabajo",
-    url: "https://www.emol.cl"
+
 }];
 
 var trabajosLoreto= [{
-    figure: "media/trabajo-01-estructuras.jpg",
+    figure: "media/trabajo-01-lolo.jpg",
     alt: "Descripción de la imagen",
     title: "Cyl",
-    about: "Banca de madera de terciado de 3 mm, pintada y sellada con carbonileo",
-    url: "http://www.instagram.com/brnwld"
+    about: "Banca fabricada a partir de placas de terciado de 3 milímetros. La banca tiene solo dos puntos de apoyo y reforzada por un sistema de costillas y ensambles, y estructurada con cables zip.",
+
 }, {
-    figure: "media/trabajo-02-taller.jpg",
+    figure: "media/trabajo-02-lolo.jpg",
     alt: "Descripción de la imagen",
-    title: "Título de segundo trabajo",
-    about: "Texto simulado de descripción de segundo trabajo",
-    url: "https://www.emol.cl"
+    title: "PEP",
+    about: "Sistema desarrollado para Ilko, que consiste en un juego de porcionadores de alimentos para niños de 2 a 5 años, en los cuales están medidas las porciones necesarias de carbohidratos, proteínas, verduras y frutas que necesita un niño dentro de ese rando de edad.",
+
 }, {
-    figure: "media/trabajo-03-zoologico.jpg",
-    alt: "Descripción de la imagen",
-    title: "Dispositivo de Enriquecimiento Animal para Servales",
-    about: "Texto simulado de descripción de tercer trabajo",
-    url: "https://www.emol.cl"
-}, {
-    figure: "media/trabajo-01-estructuras.jpg",
-    alt: "Descripción de la imagen",
-    title: "Cyl",
-    about: "Banca de madera de terciado de 3 mm, pintada y sellada con carbonileo",
-    url: "http://www.instagram.com/brnwld"
-}, {
-    figure: "media/trabajo-02-taller.jpg",
-    alt: "Descripción de la imagen",
-    title: "Título de segundo trabajo",
-    about: "Texto simulado de descripción de segundo trabajo",
-    url: "https://www.emol.cl"
-}, {
-    figure: "media/trabajo-03-zoologico.jpg",
+    figure: "media/trabajo-03-lolo.jpg",
     alt: "Descripción de la imagen",
     title: "Dispositivo de Enriquecimiento Animal para Servales",
-    about: "Texto simulado de descripción de tercer trabajo",
-    url: "https://www.emol.cl"
+    about: "Dispositivo creado con el propósito de darle uso al espacio áreo en el recinto de los servales, aumentar su actividad física y reducir las conductas estereotípicas comunes en animales en cautiverio. Éste trabajo se realizó en conjunto con PARQUEMET",
+
+}, {
+    figure: "media/trabajo-04-lolo.jpg",
+    alt: "Descripción de la imagen",
+    title: "Proyecto de Iluminación para Puente Condell",
+    about: "Proyecto que buscaba modificar la iluminación del Puente Condell, para resaltar la idea romántica que tienen los transeúntes sobre el mismo y poner en valor los candados que la gente ha ido añadiendo con el paso del tiempo.",
+
+}, {
+    figure: "media/trabajo-05-lolo.jpg",
+    alt: "Descripción de la imagen",
+    title: "CHIQUEFUERTE",
+    about: "Producto resultante de un proyecto de fusión de comida típica Chilena y Mexicana. Consistente en una pasta de chorizo apanada en tortilla y acompañada de una salsa de chilaquiles con pebre.",
+
+}, {
+    figure: "media/trabajo-06-lolo.jpeg",
+    alt: "Descripción de la imagen",
+    title: "Lettering",
+    about: "Muestra de uno de los letterings realizados para la campaña publicitaria 'La ternura viene de adentro', de chocolates Milka, el año 2018",
+
 }];
 
 var trabajosBelen= [{
-    figure: "media/trabajo-01-estructuras.jpg",
+    figure: "media/trabajo-01-belen.jpg",
     alt: "Descripción de la imagen",
-    title: "Cyl",
-    about: "Banca de madera de terciado de 3 mm, pintada y sellada con carbonileo",
-    url: "http://www.instagram.com/brnwld"
+    title: "Raco",
+    about: "Texto simulado de descrición de primer trabajo",
+
 }, {
-    figure: "media/trabajo-02-taller.jpg",
+    figure: "media/trabajo-02-belen.jpg",
     alt: "Descripción de la imagen",
     title: "Título de segundo trabajo",
     about: "Texto simulado de descripción de segundo trabajo",
-    url: "https://www.emol.cl"
+
 }, {
     figure: "media/trabajo-03-zoologico.jpg",
     alt: "Descripción de la imagen",
     title: "Dispositivo de Enriquecimiento Animal para Servales",
     about: "Texto simulado de descripción de tercer trabajo",
-    url: "https://www.emol.cl"
+
 }, {
     figure: "media/trabajo-01-estructuras.jpg",
     alt: "Descripción de la imagen",
     title: "Cyl",
     about: "Banca de madera de terciado de 3 mm, pintada y sellada con carbonileo",
-    url: "http://www.instagram.com/brnwld"
+
 }, {
     figure: "media/trabajo-02-taller.jpg",
     alt: "Descripción de la imagen",
     title: "Título de segundo trabajo",
     about: "Texto simulado de descripción de segundo trabajo",
-    url: "https://www.emol.cl"
+
 }, {
     figure: "media/trabajo-03-zoologico.jpg",
     alt: "Descripción de la imagen",
     title: "Dispositivo de Enriquecimiento Animal para Servales",
     about: "Texto simulado de descripción de tercer trabajo",
-    url: "https://www.emol.cl"
+
 }];
 // LUEGO CREAMOS PARTES DEL SITIO CON CADA DATO CONTENIDO EN LA VARIABLE
 $.each(trabajosValentina, function(i, val) {
